16c380f46ae65cf34a661af14a3f15450568c3bc
sateeshkostu <sateeshkostu45@gmail.com>
2025-02-24 21:50:03 +0530

    ui changes

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@
 //  * Sample React Native App
 //  * https://github.com/facebook/react-native
 //  *
-//  * 
+//  *
 //  */
 
 // import React from 'react';
@@ -29,22 +29,54 @@
 
 // export default App;
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import OtpScreen from './src/screens/OtpScreen';
 import Dashboard from './src/screens/Dashboard';
-import AddSite from './src/screens/AddSite';
-import UserLoc from './src/components/location'
+import UserLoc from './src/components/location';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  useEffect(() => {
+    const checkAppStatus = async () => {
+      try {
+        const userStatus = await AsyncStorage.getItem('isLoggedIn');
+        console.log('userStatus', userStatus);
+        if (!userStatus) {
+          await AsyncStorage.setItem('isLoggedIn', 'false');
+          setIsLoggedIn(false);
+        } else if (userStatus === 'true') {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (err) {
+        console.log(err);
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkAppStatus();
+  }, []);
+
+  if (isLoggedIn === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color={'red'} />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName={isLoggedIn ? "Dashboard" : "Login"} screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Otp" component={OtpScreen} />
